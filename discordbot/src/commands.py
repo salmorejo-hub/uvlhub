41948755c0c664fb2743f6429ae3c92c5af24863
JosueRodLop <josrodlop19@alum.us.es>
2024-11-11 01:39:54 +0100

    fix: Add help information to hello command Ref #13

--- discordbot/src/commands.py
+++ discordbot/src/commands.py
@@ -10,11 +10,12 @@ async def h(ctx):
     await ctx.send("Hello!")
     
     
-@client.command()
-async def help(ctx):
+@client.command(help="Say hello to check if the bot is working correctly")
+async def helpme(ctx):
     response = """
     **List of commands:**
     - !h: Say hello to check if the bot is working correctly
     - !help: Show this message to list all available commands
     """
     await ctx.send(response)
+    
