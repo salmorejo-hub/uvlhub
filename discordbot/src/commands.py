62e9f2979b6e846f68deef8039d497e89e0b9565
JosueRodLop <josrodlop19@alum.us.es>
2024-11-11 01:22:43 +0100

    feat: Add help command Ref #13

--- discordbot/src/commands.py
+++ discordbot/src/commands.py
@@ -8,3 +8,13 @@ async def on_ready():
 @client.command()
 async def h(ctx):
     await ctx.send("Hello!")
+    
+    
+@client.command()
+async def help(ctx):
+    response = """
+    **List of commands:**
+    - !h: Say hello to check if the bot is working correctly
+    - !help: Show this message to list all available commands
+    """
+    await ctx.send(response)
