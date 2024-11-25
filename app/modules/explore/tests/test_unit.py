17d0d494b8358fdba9f48371da22d1000e9384e2
isacancor <isacancor@alum.us.es>
2024-11-25 18:43:02 +0100

    fix: Fix explore tests lint

--- app/modules/explore/tests/test_unit.py
+++ app/modules/explore/tests/test_unit.py
@@ -3,7 +3,7 @@ import pytest
 from app.modules.explore.services import ExploreService
 from app.modules.auth.seeders import AuthSeeder
 from app.modules.dataset.seeders import DataSetSeeder
-from datetime import datetime
+
 
 @pytest.fixture(scope='module')
 def test_client(test_client):
@@ -13,7 +13,7 @@ def test_client(test_client):
     with test_client.application.app_context():
         users = AuthSeeder()
         users.run()
-        
+
         seeder = DataSetSeeder()
         seeder.run()
 
@@ -28,8 +28,8 @@ def test_sample_assertion(test_client):
     """
     greeting = "Hello, World!"
     assert greeting == "Hello, World!", "The greeting does not coincide with 'Hello, World!'"
-    
-    
+
+
 def test_filtering_service_model_count(test_client):
     """
     Test to verify that the filtering service correctly counts the number of datasets based on the number of models.
@@ -37,7 +37,8 @@ def test_filtering_service_model_count(test_client):
     explore_service = ExploreService()
     datasets = explore_service.filter(min_number_of_models=10, max_number_of_models=60)
     expected_dataset_count = 1  # Based on the entries in seeders.py (50 and 75 models)
-    assert len(datasets) == expected_dataset_count, f"Expected {expected_dataset_count} datasets, but got {len(datasets)}"
+    assert len(datasets) == expected_dataset_count, \
+        f"Expected {expected_dataset_count} datasets, but got {len(datasets)}"
 
 
 def test_filtering_service_feature_count(test_client):
@@ -47,13 +48,18 @@ def test_filtering_service_feature_count(test_client):
     explore_service = ExploreService()
     datasets = explore_service.filter(min_number_of_features=10, max_number_of_features=30)
     expected_dataset_count = 3  # Based on the entries in seeders.py (10, 20, and 30 features)
-    assert len(datasets) == expected_dataset_count, f"Expected {expected_dataset_count} datasets, but got {len(datasets)}"
-    
+    assert len(datasets) == expected_dataset_count, \
+        f"Expected {expected_dataset_count} datasets, but got {len(datasets)}"
+
+
 def test_filtering_service_model_and_feature_count(test_client):
     """
-    Test to verify that the filtering service correctly counts the number of datasets based on the number of models and features.
+    Test to verify that the filtering service correctly counts the number of
+    datasets based on the number of models and features.
     """
     explore_service = ExploreService()
-    datasets = explore_service.filter(min_number_of_models=20, max_number_of_models=100, min_number_of_features=0, max_number_of_features=30)
+    datasets = explore_service.filter(min_number_of_models=20, max_number_of_models=100,
+                                      min_number_of_features=0, max_number_of_features=30)
     expected_dataset_count = 2  # Based on the entries in seeders.py (50 models, 20 features and 75 models, 40 features)
-    assert len(datasets) == expected_dataset_count, f"Expected {expected_dataset_count} datasets, but got {len(datasets)}"
+    assert len(datasets) == expected_dataset_count, \
+        f"Expected {expected_dataset_count} datasets, but got {len(datasets)}"
