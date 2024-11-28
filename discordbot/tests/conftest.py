c8b7bd9fccae9c0130f4f0b4a5a581e115cee952
JosueRodLop <josrodlop19@alum.us.es>
2024-11-28 15:06:17 +0100

    fix: configures pythonpath for tests Ref #34

--- discordbot/tests/conftest.py
+++ discordbot/tests/conftest.py
@@ -2,8 +2,8 @@ import pytest
 import pytest_asyncio
 import discord.ext.test as dpytest
 from discordbot.src.setup import client
-from discord.ext import commands
 import os
+from discordbot.src.setup import Base, engine
 
 @pytest.fixture(scope="session", autouse=True)
 def set_test_db_env():
@@ -17,14 +17,23 @@ async def bot(request):
     
     dpytest.configure(client)
 
-    yield client
+    # Configura los guilds
+    guild1 = dpytest.backend.make_guild("guild1")
+    guild2 = dpytest.backend.make_guild("guild2")
+    
+    # Configura los canales de texto
+    channel1 = dpytest.backend.make_text_channel("channel1", guild=guild1)
+    channel2 = dpytest.backend.make_text_channel("channel2", guild=guild2)
+    
+    # Configura los usuarios
+    user1 = dpytest.backend.make_user("user1", 1234)
+    user2 = dpytest.backend.make_user("user2", 5678)
+    
+    # Configura los miembros
+    member1 = dpytest.backend.make_member(user1, guild1)
+    member2 = dpytest.backend.make_member(user2, guild2)
+    
+    yield client, guild1, guild2, channel1, channel2, member1, member2
     
     await dpytest.empty_queue()
-
-# @pytest.fixture(scope="module", autouse=True)
-# def setup_database():
-#     # Crea las tablas en la base de datos de pruebas
-#     Base.metadata.create_all(bind=engine)
-#     yield
-#     # Elimina las tablas después de las pruebas
-#     Base.metadata.drop_all(bind=engine)
+    Base.metadata.drop_all(bind=engine)
