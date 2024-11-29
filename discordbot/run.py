58bc0fa11ee3a2b44fe3b8f4d42f30adda09a76a
JosueRodLop <josrodlop19@alum.us.es>
2024-11-29 15:12:30 +0100

    fix: Select test database when pytest Ref #34

--- discordbot/run.py
+++ discordbot/run.py
@@ -1,9 +1,10 @@
 import os
-from discordbot.src.setup import client
+from discordbot.src.setup import init
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv()
 
 token = os.getenv("BOT_TOKEN")
-
+client, engine, Base = asyncio.run(init())
 client.run(token)
--- discordbot/src/setup.py
+++ discordbot/src/setup.py
@@ -13,38 +13,42 @@ import asyncio
 
 load_dotenv()
 
-# Choose the database to use
-DATABASE = 'MARIADB_TEST_DATABASE' if os.getenv('USE_TEST_DB') == 'true' else 'MARIADB_DATABASE'
+def database_config():
+    # Choose the database to use
+    DATABASE = 'MARIADB_TEST_DATABASE' if os.getenv('USE_TEST_DB') == 'true' else 'MARIADB_DATABASE'
 
-# Configure database
-DATABASE_URL = f"mysql+pymysql://{os.getenv('MARIADB_USER')}:{os.getenv('MARIADB_PASSWORD')}@{os.getenv('MARIADB_HOSTNAME')}:{os.getenv('MARIADB_PORT')}/{os.getenv(DATABASE)}"
+    # Configure database
+    DATABASE_URL = f"mysql+pymysql://{os.getenv('MARIADB_USER')}:{os.getenv('MARIADB_PASSWORD')}@{os.getenv('MARIADB_HOSTNAME')}:{os.getenv('MARIADB_PORT')}/{os.getenv(DATABASE)}"
 
-engine = create_engine(DATABASE_URL)
+    engine = create_engine(DATABASE_URL)
 
-session_factory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Session = scoped_session(session_factory)
+    session_factory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    Session = scoped_session(session_factory)
 
-# Create database tables
-Base.metadata.create_all(bind=engine)
-
-
- # Funtion to automatically get the prefix of the server/user
-def get_prefix(bot, message):
-    id = str(message.guild.id) if message.guild else str(message.author.id)
+    # Create database tables
+    Base.metadata.create_all(bind=engine)
     
-    try:
-        db = Session()
-        server_prefix = db.query(ServerPrefix).filter(ServerPrefix.server_id == id).first()
-        if server_prefix:
-            prefix = server_prefix.prefix
-        else:
-            prefix = "!"
-        return prefix
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        return "!"
-    finally:
-        Session.remove()
+    return Session, engine, Base
+
+
+# Funtion to automatically get the prefix of the server/user
+def get_prefix_factory(Session):
+    def get_prefix(bot, message):
+        id = str(message.guild.id) if message.guild else str(message.author.id)
+        try:
+            db = Session()
+            server_prefix = db.query(ServerPrefix).filter(ServerPrefix.server_id == id).first()
+            if server_prefix:
+                prefix = server_prefix.prefix
+            else:
+                prefix = "!"
+            return prefix
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            return "!"
+        finally:
+            Session.remove()
+    return get_prefix
     
     
 
@@ -53,9 +57,11 @@ async def init():
     intents.members = True
     intents.message_content = True
     
+    Session, engine, Base = database_config()
+    
+    get_prefix = get_prefix_factory(Session)
+    
     client = commands.Bot(command_prefix=get_prefix, intents=intents)
     await setup_basic_commands(client, Session)
     await setup_token_commands(client, Session)
-    return client
-
-client = asyncio.run(init())
+    return client, engine, Base
