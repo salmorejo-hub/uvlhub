8ef7acad003b4e801f171331bf34401ad9ee4981
DavidFuentelsazRodriguez <davfuerod@alum.us.es>
2024-12-03 14:33:17 +0100

    test: Add Advanced Filtering selenium test Closes #39

--- app/modules/explore/tests/test_selenium.py
+++ app/modules/explore/tests/test_selenium.py
@@ -1,35 +1,81 @@
-from selenium.common.exceptions import NoSuchElementException
-import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.webdriver.common.action_chains import ActionChains
 
-from core.environment.host import get_host_for_selenium_testing
 from core.selenium.common import initialize_driver, close_driver
 
 
 def test_explore_index():
-
     driver = initialize_driver()
+    driver.get("http://localhost:5000/explore")
 
     try:
-        host = get_host_for_selenium_testing()
+        # Wait for the element to be visible and enabled
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "max_size"))
+        ).send_keys("3")
 
-        # Open the index page
-        driver.get(f'{host}/explore')
+        dropdown = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "size_unit"))
+        )
+        dropdown.find_element(By.XPATH, "//option[. = 'KB']").click()
 
-        # Wait a little while to make sure the page has loaded completely
-        time.sleep(4)
+        element = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "size_unit"))
+        )
+        actions = ActionChains(driver)
+        actions.move_to_element(element).click_and_hold().perform()
+        actions.move_to_element(element).release().perform()
 
-        try:
+        dropdown.find_element(By.XPATH, "//option[. = 'MB']").click()
+        dropdown.find_element(By.XPATH, "//option[. = 'GB']").click()
 
-            pass
+        dropdown = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "day"))
+        )
+        dropdown.find_element(By.XPATH, "//option[. = '5']").click()
+        dropdown.find_element(By.XPATH, "//option[. = 'Any']").click()
 
-        except NoSuchElementException:
-            raise AssertionError('Test failed!')
+        dropdown = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "month"))
+        )
+        dropdown.find_element(By.XPATH, "//option[. = 'February']").click()
+        dropdown.find_element(By.XPATH, "//option[. = 'June']").click()
+
+        dropdown = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "year"))
+        )
+        dropdown.find_element(By.XPATH, "//option[. = '2016']").click()
+        dropdown.find_element(By.XPATH, "//option[. = '2021']").click()
+
+        clear_filters_btn = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "clear-filters"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView(true);", clear_filters_btn)
+        driver.execute_script("arguments[0].click();", clear_filters_btn)
+
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "max_number_of_models"))
+        ).send_keys("46")
+
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "min_number_of_features"))
+        ).send_keys("13")
+
+        clear_filters_btn = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "clear-filters"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView(true);", clear_filters_btn)
+        driver.execute_script("arguments[0].click();", clear_filters_btn)
+
+    except NoSuchElementException:
+        raise AssertionError('Test failed!')
+
+    except ElementClickInterceptedException:
+        raise AssertionError('Element click intercepted!')
 
     finally:
-
         # Close the browser
         close_driver(driver)
-
-
-# Call the test function
-test_explore_index()
