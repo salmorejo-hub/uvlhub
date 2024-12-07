0dee06a1d569cbada52d52a0bc5c961efcb40e36
JosueRodLop <josrodlop19@alum.us.es>
2024-12-07 20:56:39 +0100

    feat: Create API to explore datasets Ref #51

--- app/modules/dataset/seeders.py
+++ app/modules/dataset/seeders.py
@@ -58,6 +58,9 @@ class DataSetSeeder(BaseSeeder):
                 ds_metrics_id=seeded_ds_metrics[i].id
             ) for i in range(4)
         ]
+        
+        # Change the title of the last dataset to addapt it to explore command tests in discord bot
+        ds_meta_data_list[3].title = 'Test dataset for discord bot'
         seeded_ds_meta_data = self.seed(ds_meta_data_list)
 
         # Create Author instances and associate with DSMetaData
