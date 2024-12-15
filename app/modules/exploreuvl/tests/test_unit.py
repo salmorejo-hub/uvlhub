3b6125b0688b39025a7ccd7967669f6708c06229
José María Portela Huerta <josporhue@alum.us.es>
2024-12-15 21:01:56 +0100

    test: Added some unit tests for dates and problems given [Closes #6]
    Also, improved tests of a parter with my changes in order to mantain it to function. It is still his, not mine. For the evaluation

--- app/modules/exploreuvl/tests/test_unit.py
+++ app/modules/exploreuvl/tests/test_unit.py
@@ -38,25 +38,62 @@ def test_filter(exploreuvl_service):
         authors = ""
         q_tags = ""
         bytes = ""
+        min_date = ""
+        max_date = ""
         publication_type = "any"
         tags = []
         advanced_search = exploreuvl_service.filter(
-            query, title, description, authors, q_tags, bytes, publication_type, tags)
+            query, title, description, authors, q_tags, bytes, min_date, max_date, publication_type, tags)
 
         assert advanced_search == mock_fm
         assert len(advanced_search) == 3
-        mock_filter.assert_called_with(query, title, description, authors, q_tags, bytes, publication_type, tags)
+        mock_filter.assert_called_with(query, title, description, authors, q_tags, bytes, min_date, max_date, publication_type, tags)
 
         query = "any"
         normal_search = exploreuvl_service.filter(
-            query, title, description, authors, q_tags, bytes, publication_type, tags)
+            query, title, description, authors, q_tags, bytes, min_date, max_date, publication_type, tags)
 
         assert normal_search == mock_fm
         assert len(normal_search) == 3
-        mock_filter.assert_called_with(query, title, description, authors, q_tags, bytes, publication_type, tags)
+        mock_filter.assert_called_with(query, title, description, authors, q_tags, bytes, min_date, max_date, publication_type, tags)
+
+def test_filter_min_date(test_client, exploreuvl_service):
+    featuremodels = exploreuvl_service.filter(min_date="2024-12-22")
+    assert len(featuremodels) == 0, f"Error al haber más de 0 modelos para fechas imposibles. Hay {len(featuremodels)}"
+
+    featuremodels = exploreuvl_service.filter(min_date="2023-08-04", bytes='5000000')
+    assert len(featuremodels) == 3, f"Error al haber algo distinto a 3 modelos para fechas imposibles, puede que por los bytes o algo. Hay {len(featuremodels)} modelos"
+
+    featuremodels = exploreuvl_service.filter(min_date="1900-01-01")
+    assert len(featuremodels) == 12, f"Error al haber algo distinto a 12 modelos para fechas imposibles. Hay {len(featuremodels)}"
+
+def test_filter_max_date(test_client, exploreuvl_service):
+    featuremodels = exploreuvl_service.filter(max_date="1900-01-01")
+    assert len(featuremodels) == 0, f"Error al haber más de 0 modelos para fechas imposibles. Hay {len(featuremodels)}"
+
+    featuremodels = exploreuvl_service.filter(max_date="2023-08-04", bytes='5000000')
+    assert len(featuremodels) == 9, f"Error al haber algo distinto a 9 modelos para fechas imposibles, puede que por los bytes o\
+        algo. Hay {len(featuremodels)} modelos"
+
+    featuremodels = exploreuvl_service.filter(max_date="2024-12-22")
+    assert len(featuremodels) == 12, f"Error al haber algo distinto a 12 modelos para fechas imposibles. Hay {len(featuremodels)}"
+
+def test_filter_min_and_max_date(test_client, exploreuvl_service):
+    featuremodels = exploreuvl_service.filter(min_date="2024-12-22", max_date="1900-01-01")
+    assert len(featuremodels) == 0, f"Error al haber más de 0 modelos para fechas imposibles. Hay {len(featuremodels)}"
+
+    featuremodels = exploreuvl_service.filter(min_date="2024-12-22", max_date="2025-01-01")
+    assert len(featuremodels) == 0, f"Error al haber más de 0 modelos para fechas imposibles. Hay {len(featuremodels)}"
+
+    featuremodels = exploreuvl_service.filter(min_date="2022-01-01", max_date="2023-12-31", bytes='5000000')
+    assert len(featuremodels) == 6, f"Error al haber algo distinto a 6 modelos para fechas imposibles, puede que por los bytes o\
+        algo. Hay {len(featuremodels)} modelos"
+
+    featuremodels = exploreuvl_service.filter(min_date="1900-01-01", max_date="2024-12-22")
+    assert len(featuremodels) == 12, f"Error al haber algo distinto a 12 modelos para fechas imposibles. Hay {len(featuremodels)}"
+
 
 # Integration test
-
 
 def test_access_filter(test_client):
 
@@ -315,3 +352,122 @@ def test_advanced_filter_negative(test_client):
     data = response.get_json()
     assert isinstance(data, list), "The response data can't be accesed"
     assert len(data) == 0, "The response doesn't have the expected result"
+
+
+def test_advanced_filter_min_date(test_client):
+
+    response = test_client.post('/exploreuvl', json={
+        'csrf_token': 'TestToken',
+        'min_date': '2024-12-22',
+        'publication_type': 'any'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list), "The response data can't be accesed"
+    assert len(data) == 0, f"The response shouldn't show {len(data)}. There must be 0 for the search."
+
+    # Antes había problemas con la unión entre estos dos atributos en la búsqueda. De ahí el test
+    response = test_client.post('/exploreuvl', json={
+        'csrf_token': 'TestToken',
+        'bytes': '5000000',
+        'min_date': '2023-08-04',
+        'publication_type': 'any'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list), "The response data can't be accesed"
+    assert len(data) == 3, f"The response shouldn't show {len(data)}. There must be 3 for the search."
+
+    response = test_client.post('/exploreuvl', json={
+        'csrf_token': 'TestToken',
+        'min_date': '1900-01-01',
+        'publication_type': 'any'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list), "The response data can't be accesed"
+    assert len(data) == 12, f"The response shouldn't show {len(data)}. There must be 12 for the search."
+
+
+def test_advanced_filter_max_date(test_client):
+
+    response = test_client.post('/exploreuvl', json={
+        'csrf_token': 'TestToken',
+        'max_date': '1900-01-01',
+        'publication_type': 'any'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list), "The response data can't be accesed"
+    assert len(data) == 0, f"The response shouldn't show {len(data)}. There must be 0 for the search."
+
+    # Antes había problemas con la unión entre estos dos atributos en la búsqueda. De ahí el test
+    response = test_client.post('/exploreuvl', json={
+        'csrf_token': 'TestToken',
+        'bytes': '5000000',
+        'max_date': '2023-08-04',
+        'publication_type': 'any'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list), "The response data can't be accesed"
+    assert len(data) == 9, f"The response shouldn't show {len(data)}. There must be 9 for the search."
+
+    response = test_client.post('/exploreuvl', json={
+        'csrf_token': 'TestToken',
+        'max_date': '2025-01-01',
+        'publication_type': 'any'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list), "The response data can't be accesed"
+    assert len(data) == 12, f"The response shouldn't show {len(data)}. There must be 12 for the search."
+
+
+def test_advanced_filter_min_and_max_date(test_client):
+
+    response = test_client.post('/exploreuvl', json={
+        'csrf_token': 'TestToken',
+        'min_date': '2024-12-22',
+        'max_date': '1900-12-22',
+        'publication_type': 'any'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list), "The response data can't be accesed"
+    assert len(data) == 0, f"The response shouldn't show {len(data)}. There must be 0 for the search."
+
+    response = test_client.post('/exploreuvl', json={
+        'csrf_token': 'TestToken',
+        'min_date': '2024-12-22',
+        'max_date': '2025-12-22',
+        'publication_type': 'any'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list), "The response data can't be accesed"
+    assert len(data) == 0, f"The response shouldn't show {len(data)}. There must be 0 for the search."
+
+    # Antes había problemas con la unión entre estos dos atributos en la búsqueda. De ahí el test
+    response = test_client.post('/exploreuvl', json={
+        'csrf_token': 'TestToken',
+        'bytes': '5000000',
+        'min_date': '2023-08-04',
+        'max_date': '2025-12-22',
+        'publication_type': 'any'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list), "The response data can't be accesed"
+    assert len(data) == 3, f"The response shouldn't show {len(data)}. There must be 3 for the search."
+
+    response = test_client.post('/exploreuvl', json={
+        'csrf_token': 'TestToken',
+        'bytes': '5000000',
+        'min_date': '1900-01-01',
+        'publication_type': 'any'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list), "The response data can't be accesed"
+    assert len(data) == 12, f"The response shouldn't show {len(data)}. There must be 12 for the search."
