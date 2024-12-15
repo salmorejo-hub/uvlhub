33864fa197d9e64075b215ce892543c49ba364c4
javmunrom <javmunrom@alum.us.es>
2024-12-15 15:58:16 +0100

    fix: unused import deleted

--- app/modules/rating/tests/test_selenium.py
+++ app/modules/rating/tests/test_selenium.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
 import time
 
 from core.environment.host import get_host_for_selenium_testing
